bd2476b725d420729e8a2048f836fab0783d9f55
kalish93 <kaleabtekalign@gmail.com>
2024-08-23 11:01:14 +0300

    feat: tool bar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
+  imports: [ MatToolbarModule,],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
