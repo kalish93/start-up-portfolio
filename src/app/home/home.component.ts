25bbc08299d9395da7bf2d2b523ffa413279efb1
yoseph <yosephhabtamu5@gmail.com>
2024-09-07 15:12:44 +0300

    header fix

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,36 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-import { ServicesComponent } from "../services/services.component";
+import { ServicesComponent } from '../services/services.component';
 import { FooterComponent } from '../footer/footer.component';
-import { ContactUsComponent } from "../contact-us/contact-us.component";
+import { ContactUsComponent } from '../contact-us/contact-us.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { CommonModule } from '@angular/common';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, ServicesComponent, FooterComponent, ContactUsComponent],
+  imports: [
+    CommonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatMenuModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    ServicesComponent,
+    FooterComponent,
+    ContactUsComponent,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
+  isDrawerOpen = false;
+  toggleDrawer() {
+    this.isDrawerOpen = !this.isDrawerOpen;
+  }
 }
